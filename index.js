ac0307601b1845bf034b6d3d14646416ad1cb61a
madisonmeccia <madisonnicole317@gmail.com>
2022-11-10 14:02:35 -0500

    ff

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
+  formid: document.getElementById("form"),
   text: document.querySelector("#text"),
   box: document.getElementById("big-black-box"),
   points: document.querySelectorAll(".point"),
 };
 
 function backgroundAndText(back, text) {
-  back.style.backgroundColor = "red"; 
-  text.textContent = "this is now a real big red box, now go look at console"; 
+  back.style.backgroundColor = "red";
+  text.textContent = "this is now a real big red box, now go look at console";
   text.style.fontSize = "40px";
 
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-});
-
-
-function changeLi() {
-  let i = 0;
-  DOMSelectors.points.forEach((el) => {
-    el.addEventListener("click", function () {
-      el.innerHTML = `${i}`; 
-      i++;
-
-      console.log(i);
-    });
+  DOMSelectors.formid.addEventListener("submit", function (backgroundAndText) {
+    backgroundAndText.preventDefault();
+    backgroundAndText(DOMSelectors.backgroundColor, DOMSelectors.textContent);
   });
-}
 
-changeLi();
+  function changeLi() {
+    let i = 0;
+    DOMSelectors.points.forEach((el) => {
+      el.addEventListener("click", function () {
+        el.innerHTML = `${i}`;
+        i++;
+
+        console.log(i);
+      });
+    });
+  }
+
+  changeLi();
+}
